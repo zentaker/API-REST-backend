7b6d1c8b3b51eea4fac88fb662ca58d28365ce78
zentaker <apalomino12@icloud.com>
2022-01-09 11:16:09 -0500

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 
 const login = async( req, res = response ) => {
@@ -37,8 +38,9 @@ const login = async( req, res = response ) => {
 
         res.json({
             ok: true,
-            token
-        })
+            token,
+            menu: getMenuFrontEnd( usuarioDB.role )
+        });
 
     } catch (error) {
         console.log(error);
@@ -86,7 +88,8 @@ const googleSignIn = async( req, res = response ) => {
         
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd( usuario.role )
         });
 
     } catch (error) {
@@ -107,10 +110,15 @@ const renewToken = async(req, res = response) => {
     // Generar el TOKEN - JWT
     const token = await generarJWT( uid );
 
+    // Obtener el usuario por UID
+    const usuario = await Usuario.findById( uid );
+
 
     res.json({
         ok: true,
-        token
+        token,
+        usuario,
+        menu: getMenuFrontEnd( usuario.role )
     });
 
 }
